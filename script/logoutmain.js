49e99be57ec04f6d24242cbb38884807522484d5
AnellSkill <angelorussell283@gmail.com>
2023-11-27 12:42:30 -0500

    create logoutmain

---
+++ script/logoutmain.js
@@ -0,0 +1,19 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
+
+import { auth } from './Firebase.js'
+
+import './logout.js'
+ 
+import 'loginCheck.js'
+
+
+onAuthStateChanged(auth, async (user) =>  {
+
+
+
+    if (user){
+
+    }else{
+
+    }
+})
