2d936283a135c1dfa1283173de5dc2301ef6e528
AnellSkill <angelorussell283@gmail.com>
2023-11-26 21:17:19 -0500

    import auth of firebase

--- script/Firebase.js
+++ script/Firebase.js
@@ -1,9 +1,8 @@
         // Import the functions you need from the SDKs you need
         import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
         // TODO: Add SDKs for Firebase products that you want to use
         // https://firebase.google.com/docs/web/setup#available-libraries
-      
+        import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
         // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
@@ -18,3 +17,4 @@
       
         // Initialize Firebase
          export const app = initializeApp(firebaseConfig);
+         export const auth = getAuth(app)
