c8436fdb32f63cd0a38221ca843f565c233bfcd8
AnellSkill <angelorussell283@gmail.com>
2023-12-04 00:12:22 -0500

    0.1 "New actualization"

--- script/loginCheck.js
+++ script/loginCheck.js
@@ -12,4 +12,33 @@ export const loginCheck = (user) => {
     loggedInLinks.forEach((link) => (link.style.display = "block"));
     loggedOutLinks.forEach((link) => (link.style.display = "none"));
   }
+
 };
+
+export const loginCheckCart = (user) => {
+  const btnAddToCart = document.querySelectorAll(".btn-add-cart");
+  const cartContainers = document.querySelectorAll("#hide-cart");
+
+  if (user) {
+      btnAddToCart.forEach((btn) => {
+          btn.style.display = "block"; // Mostrar el botón si el usuario está autenticado
+      });
+
+      cartContainers.forEach((container) => {
+          container.style.display = "block"; // Mostrar el contenedor del icono del carrito si el usuario está autenticado
+      });
+  } else {
+      btnAddToCart.forEach((btn) => {
+          btn.style.display = "none"; // Ocultar el botón si el usuario no está autenticado
+      });
+
+      cartContainers.forEach((container) => {
+          container.style.display = "none"; // Ocultar el contenedor del icono del carrito si el usuario no está autenticado
+      });
+
+      console.log("Icono del carrito oculto");
+  }
+
+  // Resto de tu código...
+};
+
--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
 import { auth } from './Firebase.js'
 import { loginCheck } from "./loginCheck.js";
+import { loginCheckCart } from './loginCheck.js'
 
 import './signUpForm.js'
 import './signInForm.js'
@@ -9,4 +10,5 @@ import './logout.js'
 
 onAuthStateChanged(auth, async (user) => {
   loginCheck(user);
+  loginCheckCart(user);
 })
